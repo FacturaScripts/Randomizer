0d26b309cf8062428335691fedf29c19f5b715c5
Antonio <elguitarraverde@hotmail.com>
2023-05-24 13:41:08 +0200

    add guard clause if the plugin is enabled
    I have added this guard clause to check that the button is only shown if the plugin is enabled.

--- Controller/Randomizer.php
+++ Controller/Randomizer.php
@@ -132,7 +132,7 @@ class Randomizer extends Base\Controller
         /// TODO: añadir tarifas
         $this->addButton('sales', 'grupos', 'generated-customer-groups', 'customer-groups', 'fas fa-users-cog', 'Random\\GruposClientes', 'GrupoClientes');
         $this->addButton('sales', 'clientes', 'generated-customers', 'customers', 'fas fa-users', 'Random\\Clientes', 'Cliente');
-        $this->addButton('sales', 'comisiones', 'generated-commissions', 'commissions', 'fas fa-percentage', 'Random\\Comisiones', 'Comision');
+        if(class_exists(\FacturaScripts\Dinamic\Model\Comision::class)) $this->addButton('sales', 'comisiones', 'generated-commissions', 'commissions', 'fas fa-percentage', 'Random\\Comisiones', 'Comision');
         $this->addButton('sales', 'presupuestoscli', 'generated-customer-estimations', 'estimations', 'fas fa-copy', 'Random\\PresupuestosClientes', 'PresupuestoCliente');
         $this->addButton('sales', 'pedidoscli', 'generated-customer-orders', 'orders', 'fas fa-copy', 'Random\\PedidosClientes', 'PedidoCliente');
         $this->addButton('sales', 'albaranescli', 'generated-customer-delivery-notes', 'delivery-notes', 'fas fa-copy', 'Random\\AlbaranesClientes', 'AlbaranCliente');
